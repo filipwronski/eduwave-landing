a4bb928d54f30f2cc3de7e8bd0e1472314097ec2
Filip Wronski <filip.wronski@morizon-gratka.pl>
2025-12-16 18:32:00 +0100

    fix: remove router, fix switch version

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app" class="min-h-screen bg-white">
-    <router-view />
+    <LandingPage v-if="!isV2" />
+    <LandingPage2 v-else />
   </div>
 </template>
 
 <script setup>
-// Router is configured in main.js
+import { ref, onMounted } from 'vue'
+import LandingPage from './components/LandingPage.vue'
+import LandingPage2 from './components/LandingPage2.vue'
+
+// Check if v2 parameter is present in URL
+const isV2 = ref(false)
+
+const checkV2Parameter = () => {
+  const urlParams = new URLSearchParams(window.location.search)
+  isV2.value = urlParams.has('v2')
+}
+
+onMounted(() => {
+  checkV2Parameter()
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import './style.scss'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
